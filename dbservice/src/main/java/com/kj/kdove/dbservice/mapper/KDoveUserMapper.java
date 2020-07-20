581c450655aeed0887a95ea0dbaa8e631d188169
bestksy <Kj1998131>
2020-07-20 19:45:38 +0800

    1.新增tk.mybatis KDoveUser mapper接口

---
+++ dbservice/src/main/java/com/kj/kdove/dbservice/mapper/KDoveUserMapper.java
@@ -0,0 +1,7 @@
+package com.kj.kdove.dbservice.mapper;
+
+import com.kj.kdove.commons.domain.KDoveUser;
+import tk.mybatis.mapper.MyMapper;
+
+public interface KDoveUserMapper extends MyMapper<KDoveUser> {
+}
