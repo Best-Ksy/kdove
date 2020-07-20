e4b6e3be6435c81c4ad6b2d0a026d90cc07149bb
bestksy <Kj1998131>
2020-07-20 19:28:40 +0800

    1.用户表实体类 2.解析json相关依赖，配合tkmybatis

---
+++ commons/src/main/java/com/kj/kdove/commons/domain/KDoveUser.java
@@ -0,0 +1,45 @@
+package com.kj.kdove.commons.domain;
+
+import lombok.*;
+
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+@Table(name = "k_dove_user")
+public class KDoveUser implements Serializable {
+
+    private static final long serialVersionUID = 6463814946309743100L;
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(generator = "JDBC")
+    private Integer id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "`password`")
+    private String password;
+
+    @Column(name = "nickname")
+    private String nickname;
+
+    @Column(name = "ucode")
+    private String ucode;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "headurl")
+    private String headurl;
+
+    @Column(name = "regdate")
+    private Date regdate;
+
+}
